d6681d0b1361bfc22525137426ab7270196c4eed
0x00-pl <0x00.pl@gmail.com>
2017-08-22 07:51:19 +0800

    add error handler

--- doc/require.js
+++ doc/require.js
@@ -2,10 +2,11 @@
 define = function (){
     let cache = {}
 
-    function load_script(name, url, cb){
+    function load_script(name, url, cb, error_cb){
         let body = document.getElementsByTagName('head')[0]
         let script = document.createElement('script')
         script.addEventListener('load', cb)
+        script.addEventListener('error', error_cb)
         script.setAttribute('type', 'text/javascript')
         script.setAttribute('charset', 'utf-8')
         script.setAttribute('async', '')
@@ -27,23 +28,38 @@ define = function (){
         let time_handler = null
 
         let timer = new Promise((resolve,reject)=>{
-            time_handler = setTimeout(timeout, ()=>{cancle_script(name); reject('timeout') })
+            time_handler = setTimeout(timeout, ()=>{console.log('timeout'); cancle_script(name); reject('timeout') })
         })
         let loader = new Promise((resolve,reject)=>{
-            load_script(name, url, (ev)=>{
-                ev.target.addEventListener('defined', ()=>{
-                    clearTimeout(time_handler)
-                    resolve(name)
-                })
-            })
+            load_script(
+                name, url,
+                (ev)=>{
+                    ev.target.addEventListener('defined', ()=>{
+                        clearTimeout(time_handler)
+                        resolve(name)
+                    })
+                },
+                (ev)=>{
+                    reject('load script error')
+                }
+            )
         })
 
         return Promise.race([timer, loader])
     }
 
+    function resolve_single_backup(name, lib_path_list, timeout){
+        lib_path_list = lib_path_list || ['']
+        lib_path_list = lib_path_list.length===0 ? [''] : lib_path_list
+        let timeout_each = timeout / lib_path_list.length
+        let ret = resolve_single(name, lib_path_list.shift(), timeout_each)
+        lib_path_list.forEach(lib_path=>ret.catch(err=>resolve_single(name, lib_path, timeout_each)))
+        return ret
+    }
+
     function define(req, cb){
         cb = cb || (x=>x)
-        let loader_list = req.map(name=>resolve_single(name, '', 5000))
+        let loader_list = req.map(name=>resolve_single_backup(name, ['http://bad.lib/', ''], 5000))
         let self_dom = document.currentScript
         let self_name = self_dom.getAttribute('module-name')
         return Promise.all(loader_list).then(name_list => {
