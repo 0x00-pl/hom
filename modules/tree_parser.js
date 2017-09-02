4bd31058aed1f816ad1132a16a33bab4220eca15
0x00-pl <0x00.pl@gmail.com>
2017-09-02 16:40:08 +0800

    add code

--- modules/tree_parser.js
+++ modules/tree_parser.js
@@ -3,8 +3,14 @@ define([], ()=>{
     let comment_end1 = '*/'
     let comment_beg2 = '//'
     let comment_end2 = '\n'
-    let sub_tree_beg_end = []
-    let seps = []
+
+    function str_with_tag(tag, str){
+        let ret = Object.create(String.prototype)
+        function toString(){ return this.str }
+        return Object.assign(ret, { tag, str, toString })
+    }
+
+
     function shift_str(str, _end){
         let p = str.indexOf(_end)
         return [str.substring(0, p), str.substring(p)]
