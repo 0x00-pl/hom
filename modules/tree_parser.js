0ff6ca0bccb7a404974632b455a8cc164ba5f5d3
0x00-pl <0x00.pl@gmail.com>
2017-09-03 03:36:21 +0800

    add tagging_comment function

--- modules/tree_parser.js
+++ modules/tree_parser.js
@@ -1,20 +1,40 @@
 define([], ()=>{
-    let comment_beg1 = '/*'
-    let comment_end1 = '*/'
-    let comment_beg2 = '//'
-    let comment_end2 = '\n'
-
     function str_with_tag(tag, str){
         let ret = Object.create(String.prototype)
         function toString(){ return this.str }
         return Object.assign(ret, { tag, str, toString })
     }
 
+    function tagging_comment(str){
+        let comment_re = (/(?=\(\*|\*\))/)
+        let tokens = str.split(comment_re)
+        let ret = []
+        tokens.reduce(([buffer, level], v)=>{
+            if(v.startsWith('(*')){
+                level++
+                buffer.push(v)
+            } else if(v.startsWith('*)')){
+                level--
+                if(level == 0){
+                    buffer.push('*)')
+                    ret.push(str_with_tag('comment',  buffer.join('')))
+                    if(v.length > 2){
+                        ret.push(str_with_tag('any', v.substr(2)))
+                    }
+                    buffer = []
+                }else{
+                    buffer.push(v)
+                }
+            } else {
+                ret.push(str_with_tag('any', v))
+            }
+            return [buffer, level]
+        }, [[], 0])
 
-    function shift_str(str, _end){
-        let p = str.indexOf(_end)
-        return [str.substring(0, p), str.substring(p)]
+        return ret
     }
+
+
 
     function tree_parse(str, commonts){
         
