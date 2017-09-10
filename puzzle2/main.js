59f370f8ccf8cbce9449108615235bd96643e0de
0x00-pl <0x00.pl@gmail.com>
2017-09-10 10:58:13 +0800

    remove commit

--- puzzle2/main.js
+++ puzzle2/main.js
@@ -31,38 +31,6 @@ function exp_concat(prefix, p){
     }
 }
 
-// function split_01(exp){
-//     if(!(exp instanceof Array)){ return exp }
-//     if(exp[0] == '+'){
-//     } else {
-
-//         return []
-//     }
-// }
-// function merge_o1(splited){
-//     let [z, a] = splited
-//     let z_exp = exp_concat(['', 0], merge_o1(z))
-//     let a_exp = exp_concat(['', 1], merge_o1(a))
-//     return exp_union([z_exp, a_exp])
-// }
-
-// function exp_union(exp_list){
-//     exp_list = exp_list.filter(e=>{
-//         if(e instanceof Array && e.length==1){
-//             return false
-//         } else {
-//             return true
-//         }
-//     })
-
-//     if(exp_list.length == 0){
-//         return ['']
-//     } else if(exp_list.length == 1){
-//         return exp_list[0]
-//     } else {
-//         return ['+'].concat(exp_list)
-//     }
-// }
 
 function exp_add_star(exp){
     if(exp.length == 1){
@@ -74,74 +42,6 @@ function exp_add_star(exp){
     }
 }
 
-function exp_group(exp_list, a){
-    let witha = [], withouta = []
-    exp_list.forEach(p=>{
-        if(p[p.length-1] == a){
-            let new_p = p.slice(0, p.length-1)
-            witha.push(new_p)
-        } else {
-            withouta.push(p)
-        }
-    })
-    return [witha, withouta]
-}
-
-function expand_all(exp){
-    if(!(exp instanceof Array)){ return exp }
-    let ret = []
-    if(exp[0] == '+'){
-        exp.slice(1).forEach(e=>{
-            let es =  expand_all(e)
-            ret = ret.concat(es)
-        })
-    } else {
-        let prefix = exp.slice(0, exp.length-1)
-        let last = exp[exp.length-1]
-        let es = expand_all(last)
-        es.forEach(e=>{
-            let line = exp_concat(prefix, e)
-            ret.push(line)
-        })
-    }
-    return ret
-}
-
-function array_all(list, f){
-    for(let i of list){
-        if(!f(i)){ return false }
-    }
-    return true
-}
-
-function lines_pop_suffix(line_list){
-    let suffix_rev = []
-    while(true){
-        let last = line_list[0][line_list[0].length-1]
-        let same = array_all(line_list, (line)=>{
-            return line[line.length-1] == last
-        })
-        if(!same){ break }
-        suffix_rev.push(last)
-        line_list = line_list.map((line)=>line.slice(0, line.length-1))
-    }
-    return [line_list, suffix_rev.reverse()]
-}
-
-
-function line_union(line_list){
-    line_list = line_list.filter(e=>(!(e instanceof Array && e.length==1)))
-
-    if(line_list.length == 0){
-        return ['+']
-    } else if(line_list.length == 1){
-        return line_list[0]
-    } else {
-        let [diff, suffix] = lines_pop_suffix(line_list)
-        let diff_exp = diff.length==0 ? ['+'] : diff.length==1 ? diff[0] : ['+'].concat(diff)
-        return exp_concat(diff_exp, [''].concat(suffix))
-    }
-}
 
 function fd_push(fd, k, v){
     let i = fd.findIndex((v)=>(v[0]==k))
@@ -220,65 +120,6 @@ function elimit_rec(line_list, a){
         })
     }
 }
-
-// function elimit_replace(exp, a){
-//     if(!(exp instanceof Array)){ return exp }
-//     if(exp[0] == '+'){
-//         // group by if endsWith a
-//         let [witha, withouta] = exp_group(exp.slice(1), a)
-//         // console.log('with?: ', witha, withouta)
-//         // replace each without a
-//         withouta = withouta.map(e=>elimit_replace(e, a))
-//         // replace a, rebuild up
-//         let witha_exp = exp_union(witha)
-//         let endsa = witha_exp.length==1 ? ['+'] : exp_concat(witha_exp, a.exp)
-//         let each = endsa[0]=='+' ? endsa.slice(1) : [endsa]
-//         // console.log('ob1c: ', each, withouta)
-//         return exp_union([].concat(each, withouta))
-//     } else {
-//         // replace each
-//         let each = exp.slice(1).map(e=>elimit_replace(e, a))
-//         // concat each
-//         return [''].concat(each)
-//     }
-// }
-
-// function pop_relatate(exp, a){
-//     if(!(exp instanceof Array)){ return exp==a ? [[['']], ['']] : [[], exp] }
-//     if(exp[0] == '+'){
-//         // pop relate each
-//         let r_list = exp.slice(1).map(e=>pop_relatate(e, a))
-//         // merge all
-//         let rela_list = []
-//         let other_list = []
-//         r_list.map(([r,o])=>{
-//             rela_list = rela_list.concat(r)
-//             other_list.push(o)
-//         })
-//         return [rela_list, exp_union(other_list)]
-//     } else {
-//         // split prefix and last
-//         let prefix = exp.slice(0, exp.length-1)
-//         let last = exp[exp.length-1]
-//         // last pop_relatate
-//         let [rela_list, other] = pop_relatate(last, a)
-//         // concat prefix and union(last_relatate)
-//         let r = rela_list.length==0 ? [] : [exp_concat(prefix, exp_union(rela_list))]
-//         let o = other.length<=1 ? [''] : exp_concat(prefix, other)
-//         // return
-//         return [r, o]
-//     }
-// }
-
-// function elimit_rec(exp, a){
-//     // pop all endsWith a
-//     let [r, o] = pop_relatate(exp, a)
-//     // rebuild up
-//     let prefix_star = exp_add_star(exp_union(r))
-//     return exp_concat(prefix_star, o)
-// }
-
-
 
 let debug = 0
 
